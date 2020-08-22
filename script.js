12f2fd6a6dcedacacb9f9c8fccb9ff73dac89b86
Timmy Cho <chotimmysf@gmail.com>
2020-08-22 11:49:38 -0700

    display correct words issue

--- script.js
+++ script.js
@@ -4,3 +4,43 @@ const playAgainBtn = document.getElementById("play-again");
 const popup = document.getElementById("popup-container");
 const notification = document.getElementById("notification-container");
 const finalMessage = document.getElementById("final-message");
+
+const figureParts = document.querySelectorAll(".figure-part");
+
+const words = [
+  "macaron",
+  "music",
+  "crossiant",
+  "tangerine",
+  "necklace",
+  "coffeescript",
+  "coffee",
+];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = ["m"];
+const wrongLetters = [];
+
+// Show hidden word
+function displayWord() {
+  wordEl.innerHTML = `
+    ${selectedWord
+      .split("")
+      .map(
+        (letter) => `
+            <span class="letter">
+                ${correctLetters.includes(letter) ? letter : ""}
+            </span>
+        `
+      )
+      .join("")}
+  `;
+
+  console.log(wordEl.innerText);
+
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = "GG YOU WON";
+    popup.style.display = "flex";
+  }
+}
